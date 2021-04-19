2fa838ad459e7a0a158f0855c1071aed2e22d37c
JosiCayon <josicayon93@gmail.com>
2021-04-19 13:57:04 +0200

    Comenzamos con React

---
+++ Tema_6_React/cheatsheet/my-app/src/components/FirstComponent.js
@@ -0,0 +1,13 @@
+export default function FirstComponent() {
+    return (
+        <div>
+            <h3>Título</h3>
+            <small>19 de Abril de 2021</small>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, debitis?</p>
+
+            
+        </div>
+    )
+}
+
+//export default FirstComponent; alternativa
